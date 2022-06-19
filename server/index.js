1f9af8856bfc3be271a41a30704760d32990bb32
Carly-Freedman <freedmcb0023@student.laccd.edu>
2022-06-19 16:03:21 -0700

    Clean up

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 //Starting point of app
+const tasks = require("./routes/tasks");
 const connection = require("./db")
 const cors = require("cors");
 const express = require("express");
+const app = express();
 
 connection();
-const app = express();
+
 app.use(express.json());
 app.use(cors);
+
+app.use("/api/tasks", tasks);
+
 const port = process.env.PORT || 8080;
 app.listen(port, ()=>console.log('Listening on port ${port}...'));
